d16fe8d367d67049eb3c50d77674e1e06c7e53d8
A00304901 <163214232+A00304901@users.noreply.github.com>
2024-03-28 20:03:07 +0530

    Decorator.cs

---
+++ Decorator_Design_Pattern/Decorator.cs
@@ -0,0 +1,16 @@
+namespace Decorator_Design_Pattern;
+
+public abstract class Decorator<T> : IComponent<T>
+{
+    protected IComponent<T> component;
+
+    public Decorator(IComponent<T> component)
+    {
+        this.component = component;
+    }
+
+    public virtual T GetText()
+    {
+        return component.GetText();
+    }
+}
