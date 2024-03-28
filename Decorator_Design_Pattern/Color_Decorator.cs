8306a309f9e624c64a1c62fdd8186a9fdc42eab4
A00304901 <163214232+A00304901@users.noreply.github.com>
2024-03-28 21:18:06 +0530

    Color_Decorator.cs

--- Decorator_Design_Pattern/Color_Decorator.cs
+++ Decorator_Design_Pattern/Color_Decorator.cs
@@ -5,6 +5,6 @@ public class ColorDecorator : Decorator<string>
     public ColorDecorator(Component_Interface<string> component) : base(component) { }
     public override string GetText()
     {
-        return $"\u001b[45m{base.GetText()}\u001b[0m";
+        return $"\u001b[36m\u001b[43m{base.GetText()}\u001b[0m";
     }
 }
